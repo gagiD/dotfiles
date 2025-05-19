64f01028892df4a3834374b6896145ad68f038c3
gagiD <dino.gaguliceldin@gmail.com>
2025-05-19 02:28:47 +0200

    nvim conf

--- neovim/.config/nvim/lua/plugins/disabled.lua
+++ neovim/.config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,4 @@
 return {
   -- disable flash
   { "folke/flash.nvim", enabled = false },
-
-  -- tmp disable
-  -- { "Exafunction/codeium.nvim", enabled = true },
 }
--- neovim/.config/nvim/lua/plugins/none-ls.lua
+++ neovim/.config/nvim/lua/plugins/none-ls.lua
@@ -3,22 +3,63 @@ return {
     "nvimtools/none-ls.nvim",
     event = "VeryLazy",
     dependencies = {
-      "mason.nvim",
       "davidmh/cspell.nvim",
     },
     opts = function(_, opts)
       local cspell = require("cspell")
 
+      local config = {
+        cspell_config_dirs = { "~/.config/cspell" },
+        decode_json = function(cspell_str)
+          local result = vim.json.decode(cspell_str)
+
+          local found = false
+          for _, path in ipairs(result.import) do
+            if path == "/home/gagi/.config/cspell/cspell.json" then
+              found = true
+              break
+            end
+          end
+
+          if found then
+            result.language = nil
+          end
+
+          return result
+        end,
+        encode_json = function(tbl)
+          local found = false
+          for _, path in ipairs(tbl.import) do
+            if path == "/home/gagi/.config/cspell/cspell.json" then
+              found = true
+              break
+            end
+          end
+
+          if found then
+            tbl.language = nil
+          end
+
+          return vim.fn.json_encode(tbl)
+        end,
+      }
+
       opts.sources = opts.sources or {}
       table.insert(
         opts.sources,
         cspell.diagnostics.with({
+          config = config,
           diagnostics_postprocess = function(diagnostic)
-            diagnostic.severity = vim.diagnostic.severity.HINT
+            diagnostic.severity = vim.diagnostic.severity.INFO
           end,
         })
       )
-      table.insert(opts.sources, cspell.code_actions)
+      table.insert(
+        opts.sources,
+        cspell.code_actions.with({
+          config = config,
+        })
+      )
     end,
   },
 }
